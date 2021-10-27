406afd669ebcf3051bd520c7afb5164df551eb24
MarHer00 <89139260+MarHer00@users.noreply.github.com>
2021-10-27 14:16:21 +0200

    Update Songverwaltung.java

--- src/Songverwaltung.java
+++ src/Songverwaltung.java
@@ -1,4 +1,19 @@
-
 public class Songverwaltung {
-
+	Song[] songs = new Song[5];
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public void fuegeSongEin() {
+		
+	}
+	
+	public void gibAlleSongsAus() {
+		for (int i = 0; i < songs.length; i++) {
+			if(songs[i] != null) {
+				System.out.println(songs[i].toString());
+			}
+		}
+	}
 }
